8166770c5b597630e8132b90e5d70f590fd924a7
INDRESH SINGH <singhindresh400@gmail.com>
2022-10-26 10:45:00 +0530

    show product in cart

--- EcommerceWebsite/ecomm.js
+++ EcommerceWebsite/ecomm.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded',dispalyAfterRefreshingPage)
 async function dispalyAfterRefreshingPage()
 {
     //e.preventDefault()
-    let waitForGettingDataFromBackend=await axios.get('http://localhost:3500/get-user')
+    let waitForGettingDataFromBackend=await axios.get('http://localhost:3500/get-user-data')
     let dataFromBackend=waitForGettingDataFromBackend.data
-    console.log(dataFromBackend)
+    //console.log(dataFromBackend)
     let elementToPresentData=document.getElementById('gadgets')
     let output=""
 
@@ -14,7 +14,7 @@ async function dispalyAfterRefreshingPage()
         output=output+
         
         `<div ><h4>${dataFromBackend[i].title}</h4><img src=${dataFromBackend[i].imageurl}>
-           <label for=shoes>Rs-${dataFromBackend[i].price}</label><button id=${dataFromBackend[i].id} type=button  >ADD TO CART</button>
+           <label for=shoes>Rs-${dataFromBackend[i].price}</label><button id=${dataFromBackend[i].id} type=button>ADD TO CART</button>
            </div>`
         
          
@@ -51,7 +51,7 @@ async function cartItems(e)
     //console.log(waitForGettingData)
     createNotification(waitForGettingData.data.title)
     
-    document.getElementById('list').innerHTML=document.getElementById('list').innerHTML+`<li>Price-${waitForGettingData.data.price} Title-${waitForGettingData.data.title}<img src=${waitForGettingData.data.imageurl}></li>`
+    //document.getElementById('list').innerHTML=document.getElementById('list').innerHTML+`<li>Price-${waitForGettingData.data.price} Title-${waitForGettingData.data.title}<img src=${waitForGettingData.data.imageurl}></li>`
     
     
 }
@@ -92,6 +92,55 @@ function displayingCartButton()
 
 }
 
+async function addToCart(a,b,c,d)
+{
+    const user={
+        id:a,
+        title:b,
+        imageurl:c,
+        price:d
+
+    }
+    const waitForPostingToCartDataBase=await axios.post(`http://localhost:3500/post-cart`,user)
+    console.log(waitForPostingToCartDataBase)
+}
+
+document.getElementById('cart').addEventListener('click',getDataFromBackend)
+
+async function  getDataFromBackend()
+{
+    let waitForGettingDataOfCart=await axios.get('http://localhost:3500/get-user')
+    //console.log(waitForGettingDataOfCart.data)
+    //document.getElementById('list').innerHTML=`<li>Price-${waitForGettingDataOfCart.data.price} Title-${waitForGettingDataOfCart.data.title}</li>`
+    let output=""
+    for(let i=0;i<waitForGettingDataOfCart.data.length;i=i+1)
+    {
+       output=output+`<li>Price-${waitForGettingDataOfCart.data[i].price} Title-${waitForGettingDataOfCart.data[i].title}<img src=${waitForGettingDataOfCart.data[i].imageurl}></li>`
+
+    }
+    document.getElementById('list').innerHTML=output
+
+}
+document.getElementById('gadgets').addEventListener('click',storeDataInCartDataBase)
+
+async function storeDataInCartDataBase(e)
+{
+    const whichButtonGotClicked=e.target.id
+   const waitForGettingData=await axios.get(`http://localhost:3500/get-user/${whichButtonGotClicked}`)
+   //console.log(waitForGettingData)
+
+    //await axios.get()
+    const user={
+        id:waitForGettingData.data.id,
+        imageurl:waitForGettingData.data.imageurl,
+        price:waitForGettingData.data.price,
+        title:waitForGettingData.data.title
+
+
+    }
+
+    const waitForPostingDataToCartDataBase=await axios.post('http://localhost:3500/post-cart',user)
+}
 
 
 
